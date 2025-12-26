e3697dce584b4737c5e6b034df12ae1f4a368a0e
Lukas Fillieux <lukas.fillieux@student.vives.be>
2025-12-26 18:19:44 +0100

    de User Interface toegevoegd voor alle oefeningen te tonen

--- app/ui.py
+++ app/ui.py
@@ -9,3 +9,8 @@ def add_exercise_flow():
     spier = ask("Spiergroep")
     oefening_toevoegen(naam, spier)
     print(" Oefening toegevoegd!")
+    
+def show_exercises_flow():
+    print("\n--- Alle oefeningen ---")
+    for ex in alle_oefeningen():
+        print(f"{ex.id}. {ex.naam} ({ex.spier})")
