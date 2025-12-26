bb2d84ebbf2148031c39366c1a2c888f123dfecd
Lukas Fillieux <lukas.fillieux@student.vives.be>
2025-12-26 12:10:24 +0100

    nieuw pad aangemaakt voor mijn databaselocatie

--- app/database.py
+++ app/database.py
@@ -5,3 +5,8 @@ import os
 def load_settings():
     with open("settings.json", "r") as f:
         return json.load(f)
+
+def get_connection():
+    settings = load_settings()
+    path = settings["database_path"]
+    return sqlite3.connect(path)
