b26b9244995f7a5d9765a7032522807b5d1dbc20
Lukas Fillieux <lukas.fillieux@student.vives.be>
2025-12-26 18:32:46 +0100

    de User Interface toegevoegd voor sets aan te maken

--- app/ui.py
+++ app/ui.py
@@ -14,3 +14,12 @@ def show_exercises_flow():
     print("\n--- Alle oefeningen ---")
     for ex in alle_oefeningen():
         print(f"{ex.id}. {ex.naam} ({ex.spier})")
+        
+def add_set_flow():
+    print("\n--- Nieuwe set ---")
+    oefening_id = ask("Exercise ID")
+    gewicht = ask("Gewicht (kg)")
+    herhalingen = ask("Herhalingen")
+    datum = ask("Datum (YYYY-MM-DD)")
+    set_toevoegen(oefening_id, gewicht, herhalingen, datum)
+    print(" Set toegevoegd!")
