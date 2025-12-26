485aa66b071aff15a533abbe0349082ce7234bc9
Lukas Fillieux <lukas.fillieux@student.vives.be>
2025-12-26 12:30:02 +0100

    de connectie opgezet en 2 tables aangemaakt met elk hun kolommen en een id zodat je een oefening aan een set kan koppelen

--- app/database.py
+++ app/database.py
@@ -10,3 +10,23 @@ def get_connection():
     settings = load_settings()
     path = settings["database_path"]
     return sqlite3.connect(path)
+
+def init_db():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS oefeningen (
+                      id INTEGER PRIMARY KEY AUTOINCREMENT,
+                      naam TEXT NOT NULL,
+                      spiergroep TEXT)""")
+
+    cursor.execute("""CREATE TABLE IF NOT EXISTS workout_sets (
+                      id INTEGER PRIMARY KEY AUTOINCREMENT,
+                      oefening_id INTEGER,
+                      gewicht REAL,
+                      herhalingen INTEGER,
+                      datum TEXT,
+                      FOREIGN KEY(oefening_id) REFERENCES oefeningen(id))""")
+
+    conn.commit()
+    conn.close()
